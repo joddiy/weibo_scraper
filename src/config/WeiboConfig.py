3890c1a8b68e6bf7e85f4cb9613163ae48fe5a71
joddiy <joddiy@qq.com>
2017-09-02 21:29:04 +0800

    +ignore

--- src/config/WeiboConfig.py
+++ src/config/WeiboConfig.py
@@ -19,7 +19,6 @@ LOGIN_URL = 'https://passport.weibo.cn/signin/login'
 
 PHANTOM_JS_PATH = '/Users/joddiyzhang/Code/JS/phantomjs-2.1.1-macosx/bin/phantomjs'
 
-COOKIES_SAVE_PATH = './config/cookies.pkl'
+COOKIES_SAVE_PATH = './cache/cookies.pkl'
 
-ACCOUNTS_PATH = './cache/account.json'
-
+ACCOUNTS_PATH = './cache/accounts.json'
