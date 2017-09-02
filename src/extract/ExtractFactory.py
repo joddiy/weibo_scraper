a243e484bf5f37699def627a8af65097a72ea0e7
joddiy <joddiy@qq.com>
2017-09-02 22:38:04 +0800

    commit

--- src/extract/ExtractFactory.py
+++ src/extract/ExtractFactory.py
@@ -14,29 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
-import argparse
-
-from src.extract.ExtractFactory import ExtractFactory
+from src.extract.WeiboExtract import WeiboExtract
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='WeiBo Scraper. Joddiy@qq.com')
+class ExtractFactory(object):
+    def factory(name):
+        if name == "weibo":
+            return WeiboExtract()
 
-    help_ = 'debug mode for develop. set 1 on, set 0 off.'
-    parser.add_argument('-d', '--debug', default='1', help=help_)
+        assert 0, "Bad shape creation: " + name
 
-    args_ = parser.parse_args()
-    return args_
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    if args.debug == '1':
-        extract = ExtractFactory.factory('weibo')
-        extract.run()
-
-    elif args.debug == '0':
-        pass
-
-    else:
-        print('debug mode error, set 1 on, set 0 off.')
+    factory = staticmethod(factory)
--- src/extract/WeiboExtract.py
+++ src/extract/WeiboExtract.py
@@ -14,29 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
-import argparse
-
-from src.extract.ExtractFactory import ExtractFactory
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='WeiBo Scraper. Joddiy@qq.com')
+class WeiboExtract(object):
+    # Create based on class name:
 
-    help_ = 'debug mode for develop. set 1 on, set 0 off.'
-    parser.add_argument('-d', '--debug', default='1', help=help_)
-
-    args_ = parser.parse_args()
-    return args_
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    if args.debug == '1':
-        extract = ExtractFactory.factory('weibo')
-        extract.run()
-
-    elif args.debug == '0':
-        pass
-
-    else:
-        print('debug mode error, set 1 on, set 0 off.')
+    def run(type):
+        a = 1
